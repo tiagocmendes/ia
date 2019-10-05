a7935c297105dc160a66408e35e15a7e674992d3
tiagocmendes <tiagocmendes@ua.pt>
2019-10-05 11:59:22 +0100

    Added some exercises

--- guiao01/ex03.py
+++ guiao01/ex03.py
@@ -1,3 +1,17 @@
+import list_sum from ex01
+
+# 3.1
+def head(l):
+    if l == []:
+        return None 
+    return l[0]
+
+# 3.2
+def tail(l):
+    if len(l) < 2:
+        return None
+    return l[1:]
+
 # 3.3
 def juntar(l1, l2):
     if len(l1) != len(l2):
@@ -7,6 +21,73 @@ def juntar(l1, l2):
     
     return [(l1[0], l2[0])] + juntar(l1[1:], l2[1:])
 
+# 3.4
+def min_element(l):
+    if l == []:
+        return None
+    mini = min_element(l[1:])
+    if mini == None or l[0] < mini:
+        return l[0]
+    else:
+        return mini
+
+# 3.5
+def remainder(l):
+    if l == []:
+        return None 
+    res = remainder(l[1:])
+    if res == None:
+        return [l[0], l[1:]]
+    elif l[0] < res[0]: 
+        return [l[0], [res[0]] + res[1]]
+    else:
+        return [res[0], res[1] + [l[0]]]
+
+# 3.6
+def min_and_max(l):
+    if l == []:
+        return None
+        
+    res = min_and_max(l[1:])
+    
+    if res == None:
+        return l[0], l[0]
+
+    mini, maxi = l[0], l[0]
+    if res[0] < mini:
+        mini = res[0]
+    if res[1] > maxi:
+        maxi = res[1]
+    
+    return mini, maxi
+
+# 3.7
+def lower_two(l):
+    fst_min, re = remainder(l)
+    snd_min, re = remainder(re)
+    return fst_min, snd_min, re
+
 if __name__ == "__main__":
-    a = juntar([1,2,3],[3,4,5])
-    print(a)
+    a = head([])
+    b = head([1,2,3])
+    c = tail([]) 
+    d = tail([1,2,3])
+    e = tail([1])
+    f = juntar([1,2,3],[3,4,5])
+    g = min_element([])
+    h = min_element([3,4,6,1,-2,5])
+    i = remainder([3,-4,1,2,1,5])
+    j,k = min_and_max([1,2,5,12,6,-12,841,63])
+    l, m, n = lower_two([1,2,5,12,6,-12,841,63])
+
+    print(f"Cabeça de []: {a}")
+    print(f"Cabeça de []: {b}")
+    print(f"Cauda de []: {c}")
+    print(f"Cauda de []: {d}")
+    print(f"Cauda de []: {e}")
+    print(f"Juntar: {f}")
+    print(f"Minimo: {g}")
+    print(f"Minimo: {h}")
+    print(f"Remainder: {i}")
+    print(f"Minimum: {j}, Maximum: {k}")
+    print(f"First min: {l}, Second min: {m}, Remainder: {n}")
--- guiao01/ex04.py
+++ guiao01/ex04.py
@@ -9,17 +9,27 @@ module = lambda x,y: abs(x) < abs(y)
 
 # 4.4
 import math
-polar = lambda x, y: (math.sqrt(x*x,y*y), math.atan2(y,x))
+polar = lambda x, y: (math.sqrt(x*x + y*y), math.atan2(y,x))
 
 # 4.5
 def exerc4_5(f,g,h):
-    return lambda x, y, z: h((f,y), g(y,z))
+    return lambda x, y, z: h(f(x,y), g(y,z))
 
 nova = exerc4_5(lambda a, b: a+b, lambda a, b: a*b, lambda a, b: a < b)
 
-def quantificador_universal(l,f):
+# 4.6
+quantificador_universal = lambda l,f: True if l == [] else f(l[0]) and quantificador_universal(l[1:], f)
     # return not False in [f(e) for e in l]
-    return l == [e for e in l if f(e)]
+    #return l == [e for e in l if f(e)]
+
+# 4.7
+quantificador_existencial = lambda l, f: False if l == [] else f(l[0]) or quantificador_existencial(l[1:], f)
+
+# 4.8
+subset = lambda l1, l2: True if l1 == [] else l1[0] in l2 and subset(l1[1:], l2)
+
+# 4.9
+
 
 if __name__ == "__main__":
 
@@ -30,3 +40,9 @@ if __name__ == "__main__":
     print("{} > 0 ? {}".format(-1,positive(-1)))
     print("|{}| < |{}|? {}".format(1,2,module(1,2)))
     print("|{}| < |{}|? {}".format(-4,2,module(-4,2)))
+    print("Polar de ({},{})? {}".format(2,1,polar(2,1,)))
+    print("Quantificador universal: {} > 0? {}".format([1,2,3,-4,5],quantificador_universal([1,2,3,-4,5], positive)))
+    print("Quantificador existencial: {} > 0? {}".format([-1,-2,-3], quantificador_existencial([-1,-2,-3], positive)))
+    print("Quantificador existencial: {} > 0? {}".format([-1,-2,-3], quantificador_existencial([-1,2,-3], positive)))
+    print("Subconjunto: {} C {}? {}".format([1,2,3],[1,2,3,4,5,6], subset([1,2,3],[1,2,3,4,5,6])))
+    print("Subconjunto: {} C {}? {}".format([1,2,3],[1,2,4,5,6], subset([1,2,3],[1,2,4,5,6])))
